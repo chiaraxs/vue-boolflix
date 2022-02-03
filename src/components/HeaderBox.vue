38c01533eb05c24b0320c83c7c7313c76e496d09
Chiara Cardinale <chiaracardinale92@gmail.com>
2022-02-03 17:20:33 +0100

    Component deleted: SearchBar

--- src/components/HeaderBox.vue
+++ src/components/HeaderBox.vue
@@ -7,7 +7,14 @@
         </div>
         
        
-       <search-bar />
+        <div class="input-group pt-3">
+
+        <!-- 1. aggiungo v-model ad input con valore 'keywordSearch' riferito al titolo del film cercato tramite input -->
+        <!-- 1.1 con @search passo al padre (HeaderBox) in App.vue l'evento  -->
+        <!-- 1.2 $emit aggancia due parametri: il nome dell'avento (search) che passa al padre e il valore passato dall'input tramite v-model -->
+        <input v-model="keywordSearch" type="text" class="form-control me-3" placeholder="Search your film" aria-label="Search">
+        <button @click="$emit('search', keywordSearch)" type="button" class="btn btn-light">Search</button>
+    </div>
     </header>
 
 
@@ -15,16 +22,13 @@
 </template>
 
 <script>
-import SearchBar from './SearchBar.vue'
 
 export default {
-  components: { 
-        SearchBar
-    },
-    data(){
-
+    data() {
+        return{
+            keywordSearch: '',      // 2. salvo il valore dell'input qui -> così sarà dinamico in base alla ricerca effettuata dall'utente
+        }
     }
-    
 }
 </script>
 
@@ -42,6 +46,12 @@ export default {
         }
 
     }
+
+    .input-group{
+    width: 400px;
+    height: 50px;
+}
+
     
 }
 </style>
