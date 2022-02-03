f1c033b7cc7f82153c0880c0994dffb81d43dcb0
Chiara Cardinale <chiaracardinale92@gmail.com>
2022-02-03 18:28:55 +0100

    Added: v-if & v-else in not-found img

--- src/components/FilmsBox.vue
+++ src/components/FilmsBox.vue
@@ -1,9 +1,13 @@
 <template>
   
   <!-- 7. qui stampo i dati dei film  -->
-    <div class="film-box text-center border border-dark px-3 py-3 my-2 mx-1"> 
+    <div class="film-box text-center border border-dark my-2 mx-1"> 
       
-        <img :src="'https://image.tmdb.org/t/p/original/' + movie.poster_path">  
+        <!-- se il link dell'img ha valore === null -> stampa questa img -->
+        <img class="not-found" v-if="movie.poster_path === null" src="https://bitsofco.de/content/images/2018/12/broken-1.png" alt="">
+            
+        <!-- se l'immagine è inclusa nel movie.poster_path -> stampala -->
+        <img v-else :src="'https://image.tmdb.org/t/p/original/' + movie.poster_path">  
         <h5 class="mt-1">{{movie.title}}</h5>
         <h6>Language: {{movie.original_language}}</h6>
         <h6>{{movie.vote_avarage}}</h6>
@@ -33,6 +37,12 @@ export default {
         
     img{
         width: 100%;
+    }
+
+    .not-found{
+       
+        padding: 30px;
+      
     }
 }
 
