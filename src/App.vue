b5f04e22e9d2e872b933cf6ec1051cf94d161865
Chiara Cardinale <chiaracardinale92@gmail.com>
2022-02-03 14:12:19 +0100

    Added: axios call test

--- src/App.vue
+++ src/App.vue
@@ -8,8 +8,9 @@
 </template>
 
 <script>
-import HeaderBox from './components/HeaderBox.vue'
-import MainContent from './components/MainContent.vue'
+import HeaderBox from './components/HeaderBox.vue';
+import MainContent from './components/MainContent.vue';
+import axios from 'axios';
 
 
 
@@ -18,8 +19,19 @@ export default {
   components: {
     HeaderBox,
     MainContent,
-    
-  }
+  },
+  data(){
+    return {
+      movies: [],    // creo array vuoto che verrà popolato con i dati ricevuti dalla chiamata api
+    }
+
+  },
+  mounted(){
+    axios.get('https://api.themoviedb.org/3/search/movie?api_key=e99307154c6dfb0b4750f6603256716d&query=ritorno+al+futuro').then((response) => {
+    this.movies = response.data.results;    // chiamata api 
+    console.log (response.data.results);     // console debug
+    });
+  } 
 }
 </script>
 
