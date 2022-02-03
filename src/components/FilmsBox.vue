6994ece2672ca600a30204c2c65f4e52f5f09ac2
Chiara Cardinale <chiaracardinale92@gmail.com>
2022-02-03 18:33:05 +0100

    Changed: padding on cards films

--- src/components/FilmsBox.vue
+++ src/components/FilmsBox.vue
@@ -1,14 +1,14 @@
 <template>
   
   <!-- 7. qui stampo i dati dei film  -->
-    <div class="film-box text-center border border-dark my-2 mx-1"> 
+    <div class="film-box text-center border border-dark my-2 mx-1 py-4 px-4"> 
       
         <!-- se il link dell'img ha valore === null -> stampa questa img -->
-        <img class="not-found" v-if="movie.poster_path === null" src="https://bitsofco.de/content/images/2018/12/broken-1.png" alt="">
+        <img class="not-found px-3 py-5" v-if="movie.poster_path === null" src="https://bitsofco.de/content/images/2018/12/broken-1.png" alt="">
             
-        <!-- se l'immagine è inclusa nel movie.poster_path -> stampala -->
+        <!-- altrimenti, se l'immagine è inclusa nel movie.poster_path -> stampala -->
         <img v-else :src="'https://image.tmdb.org/t/p/original/' + movie.poster_path">  
-        <h5 class="mt-1">{{movie.title}}</h5>
+        <h5 class="mt-2">{{movie.title}}</h5>
         <h6>Language: {{movie.original_language}}</h6>
         <h6>{{movie.vote_avarage}}</h6>
         
@@ -39,11 +39,6 @@ export default {
         width: 100%;
     }
 
-    .not-found{
-       
-        padding: 30px;
-      
-    }
 }
 
 
