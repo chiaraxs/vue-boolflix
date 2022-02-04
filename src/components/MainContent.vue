070d88a4f2beeb7c95b1f4311986f4086e4be75d
Chiara Cardinale <chiaracardinale92@gmail.com>
2022-02-04 18:36:46 +0100

    Fixed: v-if v-else conditions

--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -8,7 +8,7 @@
     
     <div class="main-content d-flex justify-content-center flex-wrap">
         <cards-box v-for="movie in movies" :key="movie.id" :info="movie" :type="'movie'" />
-        <cards-box v-for="movie in movies" :key="movie.id" :info="movie" :type="'serie'" />
+        <cards-box v-for="serie in series" :key="serie.id" :info="serie" :type="'serie'" />
     </div>
 
     
